d49deda17a50e99e17800848a5269c093619cf89
Kerolos <keronakhla@gmail.com>
2016-01-03 23:15:11 -0800

    Added Xcode projects

---
+++ Spots/Spots/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spots
+//
+//  Created by Kerolos Nakhla on 1/3/16.
+//  Copyright © 2016 Kerolos Nakhla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spots/Spots/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spots
+//
+//  Created by Kerolos Nakhla on 1/3/16.
+//  Copyright © 2016 Kerolos Nakhla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
